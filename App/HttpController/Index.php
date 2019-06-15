8d80c72720e8ac1abfa6397b4c94d313dd2fcfc6
lulinyang <15655569098@163.com>
2019-06-15 19:37:42 +0800

    a

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -9,9 +9,9 @@ class Index extends Base
     public function index()
     {
         // $conf = Config::getInstance()->load('database.php');
-        var_dump(EASYSWOOLE_ROOT.'/App/Config/database.php');
-        $str = EASYSWOOLE_ROOT.'/App/Config/database.php';
-        $Conf = Config::getInstance()->loadFile(EASYSWOOLE_ROOT.'/App/Config/database.php');
+        // var_dump(EASYSWOOLE_ROOT.'/App/Config/database.php');
+        // $str = EASYSWOOLE_ROOT.'/App/Config/database.php';
+        $Conf = Config::getInstance()->getConf('database');
         // $this->response()->write('Hello World');
         $this->writeJson(200, $str, 'success');
     }
