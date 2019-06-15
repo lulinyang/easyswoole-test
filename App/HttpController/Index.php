6d0b731d3a44a31b37e01525a8fab945e849318e
lulinyang <15655569098@163.com>
2019-06-16 00:37:49 +0800

    a

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -12,15 +12,23 @@ class Index extends Base
 {
     public function index()
     {
-        $data = MysqlPool::invoke(function (MysqlObject $db) {
-            $user = new User($db);
-            //new 一个条件类,方便传入条件
-            $conditionBean = new ConditionBean();
-            $conditionBean->addWhere('name', '', '<>');
+        try {
+            $data = MysqlPool::invoke(function (MysqlObject $db) {
+                $user = new User($db);
+                //new 一个条件类,方便传入条件
+                $conditionBean = new ConditionBean();
+                $conditionBean->addWhere('name', '', '<>');
 
-            return $user->getAll($conditionBean->toArray([], SplBean::FILTER_NOT_NULL));
-        });
-        $this->writeJson(200, $data, 'success');
+                return $user->getAll($conditionBean->toArray([], SplBean::FILTER_NOT_NULL));
+            });
+            $this->writeJson(200, $data, 'success');
+        } catch (\Throwable $throwable) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, null, $throwable->getMessage());
+        } catch (PoolEmpty $poolEmpty) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, null, '没有链接可用');
+        } catch (PoolUnRegister $poolUnRegister) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, null, '连接池未注册');
+        }
     }
 
     public function test()
