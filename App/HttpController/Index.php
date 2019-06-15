8eaaa45dfdbe54f6b987190de77fa4df90362dc3
lulinyang <15655569098@163.com>
2019-06-15 19:14:56 +0800

    a

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -9,7 +9,7 @@ class Index extends Base
     public function index()
     {
         // $conf = Config::getInstance()->load('database.php');
-        $Conf = Config::getInstance()->getConf('database');
+        $Conf = Config::getInstance()->loadFile('database.php');
         // $this->response()->write('Hello World');
         $this->writeJson(200, $conf, 'success');
     }
