f974e4d789b8d3b32165392fc11cb65c267d9546
lulinyang <15655569098@163.com>
2019-06-16 00:45:25 +0800

    a

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -12,23 +12,24 @@ class Index extends Base
 {
     public function index()
     {
-        try {
-            $data = MysqlPool::invoke(function (MysqlObject $db) {
-                $user = new User($db);
-                //new 一个条件类,方便传入条件
-                $conditionBean = new ConditionBean();
-                $conditionBean->addWhere('name', '', '<>');
+        $this->writeJson(200, 'aaa', 'success');
+        // try {
+        //     $data = MysqlPool::invoke(function (MysqlObject $db) {
+        //         $user = new User($db);
+        //         //new 一个条件类,方便传入条件
+        //         $conditionBean = new ConditionBean();
+        //         $conditionBean->addWhere('name', '', '<>');
 
-                return $user->getAll($conditionBean->toArray([], SplBean::FILTER_NOT_NULL));
-            });
-            $this->writeJson(200, $data, 'success');
-        } catch (\Throwable $throwable) {
-            $this->writeJson(Status::CODE_BAD_REQUEST, null, $throwable->getMessage());
-        } catch (PoolEmpty $poolEmpty) {
-            $this->writeJson(Status::CODE_BAD_REQUEST, null, '没有链接可用');
-        } catch (PoolUnRegister $poolUnRegister) {
-            $this->writeJson(Status::CODE_BAD_REQUEST, null, '连接池未注册');
-        }
+        //         return $user->getAll($conditionBean->toArray([], SplBean::FILTER_NOT_NULL));
+        //     });
+        //     $this->writeJson(200, $data, 'success');
+        // } catch (\Throwable $throwable) {
+        //     $this->writeJson(Status::CODE_BAD_REQUEST, null, $throwable->getMessage());
+        // } catch (PoolEmpty $poolEmpty) {
+        //     $this->writeJson(Status::CODE_BAD_REQUEST, null, '没有链接可用');
+        // } catch (PoolUnRegister $poolUnRegister) {
+        //     $this->writeJson(Status::CODE_BAD_REQUEST, null, '连接池未注册');
+        // }
     }
 
     public function test()
