392075aee339f7dfa7ffd4cbcc19492a6f0961e2
lulinyang <15655569098@163.com>
2019-06-15 19:47:10 +0800

    a

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -24,7 +24,7 @@ class EasySwooleEvent implements Event
         self::loadConf(EASYSWOOLE_ROOT.'/App/Config');
         $dbConf = Config::getInstance()->getConf('database');
         $redisConf = Config::getInstance()->getConf('redis');
-        // var_dump($dbConf);
+        var_dump($redisConf);
         // $mysqlConfig = new \EasySwoole\Mysqli\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL'));
         // $poolConfig = \EasySwoole\MysqliPool\Mysql::getInstance()->register('mysql', $mysqlConfig);
         // //根据返回的poolConfig对象进行配置连接池配置项
