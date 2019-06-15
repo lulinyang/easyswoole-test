2a97ad9a98264f3a2ae5fb1e13bc28e999e945f3
lulinyang <15655569098@163.com>
2019-06-15 22:32:32 +0800

    a

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -24,12 +24,12 @@ class EasySwooleEvent implements Event
         // var_dump(EASYSWOOLE_ROOT.'/App/Config');
         self::loadConf(EASYSWOOLE_ROOT.'/App/Config');
         $dbConf = Config::getInstance()->getConf('DATABASE');
-        var_dump($dbConf);
-        // $mysqlConfig = new \EasySwoole\Mysqli\Config($dbConf['MYSQL']);
-        // $poolConfig = Mysql::getInstance()->register('mysql', $mysqlConfig);
+        // var_dump($dbConf);
+        $mysqlConfig = new \EasySwoole\Mysqli\Config($dbConf['MYSQL']);
+        $poolConfig = Mysql::getInstance()->register('mysql', $mysqlConfig);
         // //根据返回的poolConfig对象进行配置连接池配置项
-        var_dump($dbConf['MYSQL']['maxObjectNum']);
-        // $poolConfig->setMaxObjectNum($dbConf['MYSQL']['maxObjectNum']);
+        // var_dump($dbConf['MYSQL']['maxObjectNum']);
+        $poolConfig->setMaxObjectNum($dbConf['MYSQL']['maxObjectNum']);
     }
 
     public static function loadConf($ConfPath)
