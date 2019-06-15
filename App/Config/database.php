77e79a069b0d4bb770a91860395ee07258ecaeb8
lulinyang <15655569098@163.com>
2019-06-15 19:59:44 +0800

    a

--- App/Config/database.php
+++ App/Config/database.php
@@ -15,4 +15,12 @@
             'isSubQuery' => false, //是否为子查询
             'max_reconnect_times ' => '3', //最大重连次数
         ],
+        'REDIS' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'auth' => 'root',
+            'POOL_MAX_NUM' => '20',
+            'POOL_MIN_NUM' => '5',
+            'POOL_TIME_OUT' => '0.1',
+        ],
     ];
