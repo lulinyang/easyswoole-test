92be919d71217a2ec731ab4befeb6a8a9f047481
lulinyang <15655569098@163.com>
2019-06-16 18:42:13 +0800

    a

--- App/HttpController/Base.php
+++ App/HttpController/Base.php
@@ -31,6 +31,15 @@ class Base extends Controller
     public function actionNotFound()
     {
         // $this->response()->write('404');
-        $this->actionNotFound('error');
+        $this->setMethodNotAllowCallBack(function (Request $request, Response $response) {
+            $response->write('未找到处理方法');
+
+            return false; //结束此次响应
+        });
+        // $this->setRouterNotFoundCallBack(function (Request $request, Response $response) {
+        //     $response->write('未找到路由匹配');
+
+        //     return 'index'; //重定向到index路由
+        // });
     }
 }
