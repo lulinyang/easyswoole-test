d6fa91387c6f0c8c06aba4059642f6df2f02ec2d
lulinyang <15655569098@163.com>
2019-06-16 18:56:50 +0800

    a

--- App/HttpController/Base.php
+++ App/HttpController/Base.php
@@ -2,34 +2,12 @@
 
 namespace App\HttpController;
 
-use EasySwoole\Template\Render;
-use App\Utility\PlatesRender;
 use EasySwoole\Http\AbstractInterface\Controller;
 use EasySwoole\Http\Request;
 use EasySwoole\Http\Response;
 
 class Base extends Controller
 {
-    public function index()
-    {
-        // $this->actionNotFound('error');
-    }
-
-    /**
-     * 分离式渲染.
-     *
-     * @param $template
-     * @param $vars
-     */
-    private function render($template, array $vars = [])
-    {
-        $engine = new PlatesRender(EASYSWOOLE_ROOT.'/App/Views');
-        $render = Render::getInstance();
-        $render->getConfig()->setRender($engine);
-        $content = $engine->render($template, $vars);
-        $this->response()->write($content);
-    }
-
     public function actionNotFound()
     {
         $this->response()->write('404');
