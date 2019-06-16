1614922b43eee147def0fa6d13a710a978dc7b2b
lulinyang <15655569098@163.com>
2019-06-16 19:28:28 +0800

    a

--- App/HttpController/Base.php
+++ App/HttpController/Base.php
@@ -3,23 +3,11 @@
 namespace App\HttpController;
 
 use EasySwoole\Http\AbstractInterface\Controller;
-use EasySwoole\Http\Request;
-use EasySwoole\Http\Response;
 
 class Base extends Controller
 {
-    public function actionNotFound()
-    {
-        $this->response()->write('404');
-        // $this->setMethodNotAllowCallBack(function (Request $request, Response $response) {
-        //     $response->write('未找到处理方法');
-
-        //     return false; //结束此次响应
-        // });
-        // $this->setRouterNotFoundCallBack(function (Request $request, Response $response) {
-        //     $response->write('未找到路由匹配');
-
-        //     return 'index'; //重定向到index路由
-        // });
-    }
+    // public function actionNotFound()
+    // {
+    //     $this->response()->write('404');
+    // }
 }
