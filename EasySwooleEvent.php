1c61d6682d0f005da55107577c6805624fb94066
lulinyang <15655569098@163.com>
2019-06-16 15:13:56 +0800

    a

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -56,12 +56,13 @@ class EasySwooleEvent implements Event
         $conf->setType(\EasySwoole\Socket\Config::WEB_SOCKET);
         // 设置解析器对象
         $conf->setParser(new WebSocketParser());
-        // 创建 Dispatcher 对象 并注入 config 对象
-        $dispatch = new Dispatcher($conf);
-        // 给server 注册相关事件 在 WebSocket 模式下  on message 事件必须注册 并且交给 Dispatcher 对象处理
-        $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
-            $dispatch->dispatch($server, $frame->data, $frame);
-        });
+        var_dump($conf);
+        // // 创建 Dispatcher 对象 并注入 config 对象
+        // $dispatch = new Dispatcher($conf);
+        // // 给server 注册相关事件 在 WebSocket 模式下  on message 事件必须注册 并且交给 Dispatcher 对象处理
+        // $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
+        //     $dispatch->dispatch($server, $frame->data, $frame);
+        // });
     }
 
     public static function onRequest(Request $request, Response $response): bool
