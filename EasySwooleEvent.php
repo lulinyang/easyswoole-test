287e52ac00b5f0da876c8afad82aa5655d30c55a
lulinyang <15655569098@163.com>
2019-06-16 15:22:36 +0800

    a

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -58,7 +58,7 @@ class EasySwooleEvent implements Event
         $conf->setParser(new WebSocketParser());
         var_dump('aaaaa', $conf);
         // 创建 Dispatcher 对象 并注入 config 对象
-        $dispatch = new Dispatcher($conf);
+        $dispatch = new Dispatcher($conf->parser);
         // 给server 注册相关事件 在 WebSocket 模式下  on message 事件必须注册 并且交给 Dispatcher 对象处理
         $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
             $dispatch->dispatch($server, $frame->data, $frame);
