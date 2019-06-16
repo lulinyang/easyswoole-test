93c7f6bc1a9547919e49411418d75f72bf7a0597
lulinyang <15655569098@163.com>
2019-06-16 23:14:10 +0800

    aa

--- App/WebSocket/Index.php
+++ App/WebSocket/Index.php
@@ -20,7 +20,26 @@ class Index extends Controller
 
     public function who()
     {
-        $this->response()->setMessage('your fd is '.$this->caller()->getClient()->getFd());
+        /** @var \swoole_websocket_server $server */
+        $server = ServerManager::getInstance()->getSwooleServer();
+        $start = 0;
+
+        // 此处直接遍历所有FD进行消息投递
+        // 生产环境请自行使用Redis记录当前在线的WebSocket客户端FD
+        while (true) {
+            $conn_list = $server->connection_list($start, 10);
+            if (empty($conn_list)) {
+                break;
+            }
+            $start = end($conn_list);
+            foreach ($conn_list as $fd) {
+                $info = $server->getClientInfo($fd);
+                /* 判断此fd 是否是一个有效的 websocket 连接 */
+                if ($info && $info['websocket_status'] == WEBSOCKET_STATUS_FRAME) {
+                    $server->push($fd, 'http broadcast fd '.$fd.' at '.date('H:i:s'));
+                }
+            }
+        }
     }
 
     public function delay()
