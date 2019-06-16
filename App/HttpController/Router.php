b6730256fdd55f8e2876b8f119b7d0b56813385f
lulinyang <15655569098@163.com>
2019-06-16 19:46:23 +0800

    a

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -11,8 +11,10 @@ class Router extends AbstractRouter
 {
     public function initialize(RouteCollector $routeCollector)
     {
-        $this->setGlobalMode(true);
+        // $this->setGlobalMode(true);
         // $this->setGlobalMode(false);
+        $routeCollector->get('/', '/Index/index');
+        // $routeCollector->get('/test', '/Index/test');
         $this->setMethodNotAllowCallBack(function (Request $request, Response $response) {
             $response->write('未找到处理方法');
         });
@@ -20,7 +22,5 @@ class Router extends AbstractRouter
             $response->write('未找到路由匹配');
         });
         // TODO: Implement initialize() method.
-        $routeCollector->get('/', '/Index/index');
-        // $routeCollector->get('/test', '/Index/test');
     }
 }
