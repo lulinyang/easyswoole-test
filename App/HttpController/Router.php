e4d6f249c41c6e36182304c3387a4349dc720b44
lulinyang <15655569098@163.com>
2019-06-16 19:03:22 +0800

    a

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -10,6 +10,12 @@ class Router extends AbstractRouter
     public function initialize(RouteCollector $routeCollector)
     {
         // $this->setGlobalMode(true);
+        $this->setMethodNotAllowCallBack(function (Request $request, Response $response) {
+            $response->write('未找到处理方法');
+        });
+        $this->setRouterNotFoundCallBack(function (Request $request, Response $response) {
+            $response->write('未找到路由匹配');
+        });
         $routeCollector->get('/', '/Index/index');
         $routeCollector->get('/websocket', '/WebSocket/index');
     }
