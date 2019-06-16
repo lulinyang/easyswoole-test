efb62b04385d7a1038c1d528403f6c72ac54e2a3
lulinyang <15655569098@163.com>
2019-06-16 17:08:58 +0800

    a

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -61,6 +61,7 @@ class EasySwooleEvent implements Event
         var_dump('aaaaa', $conf);
         // 创建 Dispatcher 对象 并注入 config 对象
         $dispatch = new Dispatcher($conf);
+        var_dump('a===a', $dispatch);
         // 给server 注册相关事件 在 WebSocket 模式下  on message 事件必须注册 并且交给 Dispatcher 对象处理
         $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
             $dispatch->dispatch($server, $frame->data, $frame);
@@ -80,15 +81,15 @@ class EasySwooleEvent implements Event
     public static function onRequest(Request $request, Response $response): bool
     {
         // TODO: Implement onRequest() method.
-        $response->withHeader('Access-Control-Allow-Origin', '*');
-        $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        $response->withHeader('Access-Control-Allow-Credentials', 'true');
-        $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        if ($request->getMethod() === 'OPTIONS') {
-            $response->withStatus(Status::CODE_OK);
+        // $response->withHeader('Access-Control-Allow-Origin', '*');
+        // $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        // $response->withHeader('Access-Control-Allow-Credentials', 'true');
+        // $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        // if ($request->getMethod() === 'OPTIONS') {
+        //     $response->withStatus(Status::CODE_OK);
 
-            return false;
-        }
+        //     return false;
+        // }
 
         return true;
     }
