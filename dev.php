16c948b4a1d484b22c5a8cc1535dc634ce884a59
lulinyang <15655569098@163.com>
2019-06-16 16:31:27 +0800

    a

--- dev.php
+++ dev.php
@@ -27,4 +27,22 @@ return [
     'PHAR' => [
         'EXCLUDE' => ['.idea', 'Log', 'Temp', 'easyswoole', 'easyswoole.install'],
     ],
+    'CONSOLE' => [//console组件配置,完整配置可查看:http://easyswoole.com/Manual/3.x/Cn/_book/SystemComponent/Console/Introduction.html
+        'ENABLE' => true, //是否开启console
+        'LISTEN_ADDRESS' => '192.168.204.129', //console服务端监听地址
+        'HOST' => '192.168.204.129', //console客户端连接远程地址
+        'PORT' => 9500, //console服务端监听端口,客户端连接远程端口
+        'EXPIRE' => '120', //心跳超时时间
+        // 'AUTH'           => null,//鉴权密码,如不需要鉴权可设置null
+        'AUTH' => [
+            [
+                'USER' => 'root',
+                'PASSWORD' => 'root',
+                'MODULES' => [
+                    'auth', 'server', 'help', 'test',
+                ],
+                'PUSH_LOG' => true,
+            ],
+        ],
+    ],
 ];
