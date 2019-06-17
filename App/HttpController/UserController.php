c246e47e35f6ce33c86dcb7712c0b04f7fb75835
lulinyang <15655569098@163.com>
2019-06-17 22:59:49 +0800

    a

---
+++ App/HttpController/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\HttpController;
+
+use App\Model\User;
+use App\Utility\Pool\MysqlObject;
+use App\Utility\Pool\MysqlPool;
+use EasySwoole\Http\Message\Status;
+use EasySwoole\Utility\Hash;
+use EasySwoole\Component\Pool\Exception\PoolEmpty;
+use EasySwoole\Http\Request;
+
+class UserController extends Base
+{
+    public function index()
+    {
+    }
+
+    public function register(Request $request)
+    {
+        $params = $request->getRequestParam();
+        $this->writeJson(200, $params, 'success');
+        $this->response()->end();
+
+        try {
+            $data = MysqlPool::invoke(function (MysqlObject $db) {
+                $user = new User($db);
+                $password = '123456';
+                $arr = [
+                    'name' => '张三',
+                    'email' => '12228380958@qq.com',
+                    'password' => Hash::makePasswordHash($password),
+                ];
+
+                return $user->insert($arr);
+            });
+            $this->writeJson(200, $data, 'success');
+        } catch (\Throwable $throwable) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, null, $throwable->getMessage());
+        } catch (PoolEmpty $poolEmpty) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, null, '没有链接可用');
+        }
+    }
+}
