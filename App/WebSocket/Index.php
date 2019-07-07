58ce4cf94bb471614b71de0759206755d3d9a423
lulinyang <15655569098@163.com>
2019-07-07 13:17:13 +0800

    a

--- App/WebSocket/Index.php
+++ App/WebSocket/Index.php
@@ -64,7 +64,7 @@ class Index extends Controller
         /** @var \swoole_websocket_server $server */
         $server = ServerManager::getInstance()->getSwooleServer();
         $start = 0;
-
+        $params = $this->caller()->getArgs()
         // 此处直接遍历所有FD进行消息投递
         // 生产环境请自行使用Redis记录当前在线的WebSocket客户端FD
         while (true) {
@@ -77,7 +77,7 @@ class Index extends Controller
                 $info = $server->getClientInfo($fd);
                 /* 判断此fd 是否是一个有效的 websocket 连接 */
                 if ($info && $info['websocket_status'] == WEBSOCKET_STATUS_FRAME) {
-                    $server->push($fd, 'http broadcast fd '.$fd.' at '.date('H:i:s'));
+                    $server->push($fd, 'http broadcast fd '.$fd.' at '.date('H:i:s').$params);
                 }
             }
         }
